6a827b2cbec2a398f24301240bbcb498b8f95bd3
Tarun jain <thanks1998.tj@gmail.com>
2023-12-08 20:47:01 +0530

    PWM Peripheral Basic code
    Signed-off-by: Tarun jain <thanks1998.tj@gmail.com>

---
+++ Peripheral_Drivers/PWM/pwm_pal_s32k144/Debug_FLASH/src/main.d
@@ -0,0 +1,61 @@
+src/main.o: ../src/main.c \
+ C:/Users/Thank/OneDrive/Documents/s32k144_projects/pwm_pal_s32k144/board/sdk_project_config.h \
+ C:/Users/Thank/OneDrive/Documents/s32k144_projects/pwm_pal_s32k144/board/clock_config.h \
+ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/drivers/inc/clock.h \
+ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/device_registers.h \
+ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/common/s32_core_cm4.h \
+ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/S32K144/include/S32K144.h \
+ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/S32K144/include/S32K144_features.h \
+ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/devassert.h \
+ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/status.h \
+ c:\nxp\s32ds.3.4\s32ds\software\s32sdk_s32k1xx_rtm_4.0.3\platform\drivers\src\clock\s32k1xx\clock_s32k1xx.h \
+ C:/Users/Thank/OneDrive/Documents/s32k144_projects/pwm_pal_s32k144/board/pin_mux.h \
+ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/drivers/inc/pins_driver.h \
+ C:/Users/Thank/OneDrive/Documents/s32k144_projects/pwm_pal_s32k144/board/peripherals_pwm_pal_1.h \
+ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/pal/inc/pwm_pal.h \
+ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/callbacks.h \
+ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/pal/inc/pwm_pal_mapping.h \
+ C:/Users/Thank/OneDrive/Documents/s32k144_projects/pwm_pal_s32k144/board/pwm_pal_cfg.h \
+ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/drivers/inc/ftm_pwm_driver.h \
+ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/drivers/inc/ftm_common.h \
+ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/drivers/inc/ftm_common.h
+
+C:/Users/Thank/OneDrive/Documents/s32k144_projects/pwm_pal_s32k144/board/sdk_project_config.h:
+
+C:/Users/Thank/OneDrive/Documents/s32k144_projects/pwm_pal_s32k144/board/clock_config.h:
+
+C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/drivers/inc/clock.h:
+
+C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/device_registers.h:
+
+C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/common/s32_core_cm4.h:
+
+C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/S32K144/include/S32K144.h:
+
+C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/S32K144/include/S32K144_features.h:
+
+C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/devassert.h:
+
+C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/status.h:
+
+c:\nxp\s32ds.3.4\s32ds\software\s32sdk_s32k1xx_rtm_4.0.3\platform\drivers\src\clock\s32k1xx\clock_s32k1xx.h:
+
+C:/Users/Thank/OneDrive/Documents/s32k144_projects/pwm_pal_s32k144/board/pin_mux.h:
+
+C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/drivers/inc/pins_driver.h:
+
+C:/Users/Thank/OneDrive/Documents/s32k144_projects/pwm_pal_s32k144/board/peripherals_pwm_pal_1.h:
+
+C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/pal/inc/pwm_pal.h:
+
+C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/callbacks.h:
+
+C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/pal/inc/pwm_pal_mapping.h:
+
+C:/Users/Thank/OneDrive/Documents/s32k144_projects/pwm_pal_s32k144/board/pwm_pal_cfg.h:
+
+C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/drivers/inc/ftm_pwm_driver.h:
+
+C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/drivers/inc/ftm_common.h:
+
+C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/drivers/inc/ftm_common.h:
---
+++ Peripheral_Drivers/PWM/pwm_pal_s32k144/Debug_RAM/SDK/platform/pal/src/pwm/pwm_pal.d
@@ -0,0 +1,41 @@
+SDK/platform/pal/src/pwm/pwm_pal.o: \
+ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/pal/src/pwm/pwm_pal.c \
+ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/pal/inc/pwm_pal.h \
+ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/status.h \
+ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/callbacks.h \
+ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/pal/inc/pwm_pal_mapping.h \
+ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/device_registers.h \
+ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/common/s32_core_cm4.h \
+ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/S32K144/include/S32K144.h \
+ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/S32K144/include/S32K144_features.h \
+ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/devassert.h \
+ C:/Users/Thank/OneDrive/Documents/s32k144_projects/pwm_pal_s32k144/board/pwm_pal_cfg.h \
+ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/drivers/inc/ftm_pwm_driver.h \
+ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/drivers/inc/ftm_common.h \
+ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/drivers/inc/ftm_common.h
+
+C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/pal/inc/pwm_pal.h:
+
+C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/status.h:
+
+C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/callbacks.h:
+
+C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/pal/inc/pwm_pal_mapping.h:
+
+C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/device_registers.h:
+
+C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/common/s32_core_cm4.h:
+
+C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/S32K144/include/S32K144.h:
+
+C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/S32K144/include/S32K144_features.h:
+
+C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/devassert.h:
+
+C:/Users/Thank/OneDrive/Documents/s32k144_projects/pwm_pal_s32k144/board/pwm_pal_cfg.h:
+
+C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/drivers/inc/ftm_pwm_driver.h:
+
+C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/drivers/inc/ftm_common.h:
+
+C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/drivers/inc/ftm_common.h:
