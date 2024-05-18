c2c5ca5d7c06ea7342d0ecfcf5f0a9fc7420df56
kunal_gettobyte <gettobyte@gmail.com>
2024-05-18 08:49:28 +0530

    main: CAN with freeRTOS single thread and ST7789 LCD screen with HEENA images

--- Sensor_Module_Interfacing/S32K144_ST7789_Graphics_Test/Example3.2/SDK/platform/drivers/src/lpspi/lpspi_slave_driver.d
+++ Sensor_Module_Interfacing/S32K144_ST7789_Graphics_Test/Example3.2/SDK/platform/drivers/src/lpspi/lpspi_slave_driver.d
@@ -3,17 +3,17 @@ SDK/platform/drivers/src/lpspi/lpspi_slave_driver.o: \
  ../SDK/platform/drivers/inc/lpspi_slave_driver.h \
  ../SDK/platform/drivers/inc/lpspi_shared_function.h \
  ../SDK/platform/drivers/inc/edma_driver.h \
- C:/Users/gettobyte_kunal/Documents/s32k144_programs/S32K144_ST7789_Graphics_Test/SDK/platform/devices/device_registers.h \
- C:/Users/gettobyte_kunal/Documents/s32k144_programs/S32K144_ST7789_Graphics_Test/SDK/platform/devices/common/s32_core_cm4.h \
- C:/Users/gettobyte_kunal/Documents/s32k144_programs/S32K144_ST7789_Graphics_Test/SDK/platform/devices/S32K144/include/S32K144.h \
- C:/Users/gettobyte_kunal/Documents/s32k144_programs/S32K144_ST7789_Graphics_Test/SDK/platform/devices/S32K144/include/S32K144_features.h \
- C:/Users/gettobyte_kunal/Documents/s32k144_programs/S32K144_ST7789_Graphics_Test/SDK/platform/devices/devassert.h \
- C:/Users/gettobyte_kunal/Documents/s32k144_programs/S32K144_ST7789_Graphics_Test/SDK/platform/devices/status.h \
+ C:/Users/gettobyte_kunal/Documents/ElecronicsV2/Sensor_Module_Interfacing/S32K144_ST7789_Graphics_Test/SDK/platform/devices/device_registers.h \
+ C:/Users/gettobyte_kunal/Documents/ElecronicsV2/Sensor_Module_Interfacing/S32K144_ST7789_Graphics_Test/SDK/platform/devices/common/s32_core_cm4.h \
+ C:/Users/gettobyte_kunal/Documents/ElecronicsV2/Sensor_Module_Interfacing/S32K144_ST7789_Graphics_Test/SDK/platform/devices/S32K144/include/S32K144.h \
+ C:/Users/gettobyte_kunal/Documents/ElecronicsV2/Sensor_Module_Interfacing/S32K144_ST7789_Graphics_Test/SDK/platform/devices/S32K144/include/S32K144_features.h \
+ C:/Users/gettobyte_kunal/Documents/ElecronicsV2/Sensor_Module_Interfacing/S32K144_ST7789_Graphics_Test/SDK/platform/devices/devassert.h \
+ C:/Users/gettobyte_kunal/Documents/ElecronicsV2/Sensor_Module_Interfacing/S32K144_ST7789_Graphics_Test/SDK/platform/devices/status.h \
  C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/drivers/inc/clock_manager.h \
  C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/drivers/inc/clock.h \
  c:\nxp\s32ds.3.4\s32ds\software\s32sdk_s32k1xx_rtm_4.0.3\platform\drivers\src\clock\s32k1xx\clock_s32k1xx.h \
  ../SDK/rtos/osif/osif.h \
- C:/Users/gettobyte_kunal/Documents/s32k144_programs/S32K144_ST7789_Graphics_Test/SDK/platform/devices/callbacks.h \
+ C:/Users/gettobyte_kunal/Documents/ElecronicsV2/Sensor_Module_Interfacing/S32K144_ST7789_Graphics_Test/SDK/platform/devices/callbacks.h \
  ../SDK/platform/drivers/inc/interrupt_manager.h \
  ../SDK/platform/drivers/src/lpspi/lpspi_hw_access.h \
  ../SDK/platform/drivers/inc/lpspi_shared_function.h
@@ -24,17 +24,17 @@ SDK/platform/drivers/src/lpspi/lpspi_slave_driver.o: \
 
 ../SDK/platform/drivers/inc/edma_driver.h:
 
-C:/Users/gettobyte_kunal/Documents/s32k144_programs/S32K144_ST7789_Graphics_Test/SDK/platform/devices/device_registers.h:
+C:/Users/gettobyte_kunal/Documents/ElecronicsV2/Sensor_Module_Interfacing/S32K144_ST7789_Graphics_Test/SDK/platform/devices/device_registers.h:
 
-C:/Users/gettobyte_kunal/Documents/s32k144_programs/S32K144_ST7789_Graphics_Test/SDK/platform/devices/common/s32_core_cm4.h:
+C:/Users/gettobyte_kunal/Documents/ElecronicsV2/Sensor_Module_Interfacing/S32K144_ST7789_Graphics_Test/SDK/platform/devices/common/s32_core_cm4.h:
 
-C:/Users/gettobyte_kunal/Documents/s32k144_programs/S32K144_ST7789_Graphics_Test/SDK/platform/devices/S32K144/include/S32K144.h:
+C:/Users/gettobyte_kunal/Documents/ElecronicsV2/Sensor_Module_Interfacing/S32K144_ST7789_Graphics_Test/SDK/platform/devices/S32K144/include/S32K144.h:
 
-C:/Users/gettobyte_kunal/Documents/s32k144_programs/S32K144_ST7789_Graphics_Test/SDK/platform/devices/S32K144/include/S32K144_features.h:
+C:/Users/gettobyte_kunal/Documents/ElecronicsV2/Sensor_Module_Interfacing/S32K144_ST7789_Graphics_Test/SDK/platform/devices/S32K144/include/S32K144_features.h:
 
-C:/Users/gettobyte_kunal/Documents/s32k144_programs/S32K144_ST7789_Graphics_Test/SDK/platform/devices/devassert.h:
+C:/Users/gettobyte_kunal/Documents/ElecronicsV2/Sensor_Module_Interfacing/S32K144_ST7789_Graphics_Test/SDK/platform/devices/devassert.h:
 
-C:/Users/gettobyte_kunal/Documents/s32k144_programs/S32K144_ST7789_Graphics_Test/SDK/platform/devices/status.h:
+C:/Users/gettobyte_kunal/Documents/ElecronicsV2/Sensor_Module_Interfacing/S32K144_ST7789_Graphics_Test/SDK/platform/devices/status.h:
 
 C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/drivers/inc/clock_manager.h:
 
@@ -44,7 +44,7 @@ c:\nxp\s32ds.3.4\s32ds\software\s32sdk_s32k1xx_rtm_4.0.3\platform\drivers\src\cl
 
 ../SDK/rtos/osif/osif.h:
 
-C:/Users/gettobyte_kunal/Documents/s32k144_programs/S32K144_ST7789_Graphics_Test/SDK/platform/devices/callbacks.h:
+C:/Users/gettobyte_kunal/Documents/ElecronicsV2/Sensor_Module_Interfacing/S32K144_ST7789_Graphics_Test/SDK/platform/devices/callbacks.h:
 
 ../SDK/platform/drivers/inc/interrupt_manager.h:
 
