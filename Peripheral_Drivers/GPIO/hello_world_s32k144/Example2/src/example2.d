fe73a8e11e208b03bbf90ed5a96f05dea4b29e23
kunal_gettobyte <gettobyte@gmail.com>
2024-05-29 01:08:27 +0530

    FreeMaster basic code added with GPIO Peripheral

--- Peripheral_Drivers/GPIO/hello_world_s32k144/Example2/src/example2.d
+++ Peripheral_Drivers/GPIO/hello_world_s32k144/Example2/src/example2.d
@@ -1,6 +1,6 @@
 src/example2.o: ../src/example2.c \
- C:/Users/gettobyte_kunal/workspaceS32DS.3.4/hello_world_s32k144/board/sdk_project_config.h \
- C:/Users/gettobyte_kunal/workspaceS32DS.3.4/hello_world_s32k144/board/clock_config.h \
+ C:/Users/gettobyte_kunal/Documents/ElecronicsV2/Peripheral_Drivers/GPIO/hello_world_s32k144/board/sdk_project_config.h \
+ C:/Users/gettobyte_kunal/Documents/ElecronicsV2/Peripheral_Drivers/GPIO/hello_world_s32k144/board/clock_config.h \
  C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/drivers/inc/clock.h \
  C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/device_registers.h \
  C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/common/s32_core_cm4.h \
@@ -9,12 +9,12 @@ src/example2.o: ../src/example2.c \
  C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/devassert.h \
  C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/status.h \
  c:\nxp\s32ds.3.4\s32ds\software\s32sdk_s32k1xx_rtm_4.0.3\platform\drivers\src\clock\s32k1xx\clock_s32k1xx.h \
- C:/Users/gettobyte_kunal/workspaceS32DS.3.4/hello_world_s32k144/board/pin_mux.h \
+ C:/Users/gettobyte_kunal/Documents/ElecronicsV2/Peripheral_Drivers/GPIO/hello_world_s32k144/board/pin_mux.h \
  C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/drivers/inc/pins_driver.h
 
-C:/Users/gettobyte_kunal/workspaceS32DS.3.4/hello_world_s32k144/board/sdk_project_config.h:
+C:/Users/gettobyte_kunal/Documents/ElecronicsV2/Peripheral_Drivers/GPIO/hello_world_s32k144/board/sdk_project_config.h:
 
-C:/Users/gettobyte_kunal/workspaceS32DS.3.4/hello_world_s32k144/board/clock_config.h:
+C:/Users/gettobyte_kunal/Documents/ElecronicsV2/Peripheral_Drivers/GPIO/hello_world_s32k144/board/clock_config.h:
 
 C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/drivers/inc/clock.h:
 
@@ -32,6 +32,6 @@ C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/devices/status
 
 c:\nxp\s32ds.3.4\s32ds\software\s32sdk_s32k1xx_rtm_4.0.3\platform\drivers\src\clock\s32k1xx\clock_s32k1xx.h:
 
-C:/Users/gettobyte_kunal/workspaceS32DS.3.4/hello_world_s32k144/board/pin_mux.h:
+C:/Users/gettobyte_kunal/Documents/ElecronicsV2/Peripheral_Drivers/GPIO/hello_world_s32k144/board/pin_mux.h:
 
 C:/NXP/S32DS.3.4/S32DS/software/S32SDK_S32K1XX_RTM_4.0.3/platform/drivers/inc/pins_driver.h:
---
+++ Peripheral_Drivers/GPIO/hello_world_s32k144/src/example3.c
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2020 NXP
+ * All rights reserved.
+ *
+ * NXP Confidential. This software is owned or controlled by NXP and may only be
+ * used strictly in accordance with the applicable license terms. By expressly
+ * accepting such terms or by downloading, installing, activating and/or otherwise
+ * using the software, you are agreeing that you have read, and that you agree to
+ * comply with and are bound by, such license terms. If you do not agree to be
+ * bound by the applicable license terms, then you may not retain, install,
+ * activate or otherwise use the software. The production use license in
+ * Section 2.3 is expressly granted for this software.
+ */
+
+	#define PCC_CLOCK	PCC_PORTD_CLOCK
+
+    #define RED_LED_PORT PTD
+	#define RED_LED_PIN  15
+	#define GREEN_LED_PORT PTD
+	#define GREEN_LED_PIN  16
+
+    #define SWITCH2_PORT PTC
+    #define SWITCH2_PIN 13
+
+    #define SWITCH1_PORT PTC
+    #define SWITCH1_PIN 12
+
+    #define ReadSwitch2  PINS_DRV_ReadPins(SWITCH2_PORT) >> SWITCH2_PIN
+    #define ReadSwitch1 PINS_DRV_ReadPins(SWITCH1_PORT) >> SWITCH1_PIN
+
+    #define RED_LED_HIGH     PINS_DRV_SetPins(RED_LED_PORT, 1 << RED_LED_PIN);
+    #define RED_LED_LOW    PINS_DRV_ClearPins(RED_LED_PORT, 1 << RED_LED_PIN);
+    #define RED_LED_TOGGLE  PINS_DRV_TogglePins(RED_LED_PORT, 1 << RED_LED_PIN);
+
+    #define GREEN_LED_HIGH   PINS_DRV_SetPins(GREEN_LED_PORT, 1 << GREEN_LED_PIN);
+    #define GREEN_LED_LOW    PINS_DRV_ClearPins(GREEN_LED_PORT, 1 << GREEN_LED_PIN);
+    #define GREEN_LED_TOGGLE  PINS_DRV_TogglePins(GREEN_LED_PORT, 1 << GREEN_LED_PIN);
+
+#include "sdk_project_config.h"
+/* Included the FreeMAster Header file  */
+#include "freemaster.h"
+
+uint8_t Red_Led_State;
+uint8_t Green_Led_State;
+
+#define RED_LED_HIGH_VALUE 1
+#define RED_LED_LOW_VALUE 0
+#define GREEN_LED_HIGH_VALUE 1
+#define GREEN_LED_LOW_VALUE 0
+
+int main(void)
+{
+  status_t error;
+  /* Configure clocks for PORT */
+  error = CLOCK_DRV_Init(&clockMan1_InitConfig0);
+  DEV_ASSERT(error == STATUS_SUCCESS);
+  /* Set pins as GPIO */
+  error = PINS_DRV_Init(NUM_OF_CONFIGURED_PINS0, g_pin_mux_InitConfigArr0);
+  DEV_ASSERT(error == STATUS_SUCCESS);
+
+  /* Initialize FreeMASTER driver */
+  FMSTR_Init();
+
+  for (;;)
+  {
+      if(ReadSwitch1 & 0x01) // Switch1 HIGH
+      {
+    	  Red_Led_State = RED_LED_HIGH_VALUE;
+    	  RED_LED_HIGH;  // RED LED HIGH
+      }else if(!(ReadSwitch1 & 0x01)) // Switch1 LOW
+      {
+    	  Red_Led_State = RED_LED_LOW_VALUE;
+    	  RED_LED_LOW; // RED LED LOW
+      }
+
+      if(ReadSwitch2 & 0x01) // Switch2 High
+	  {
+    	  Green_Led_State = GREEN_LED_HIGH_VALUE;
+    	  GREEN_LED_HIGH; //Green LED High
+	  }else if(!(ReadSwitch2 & 0x01)) // Switch2 Low
+	  {
+		  Green_Led_State = GREEN_LED_LOW_VALUE;
+		  GREEN_LED_LOW;  //Green LED Low
+	  }
+
+      /* Handle the protocol decoding and execution */
+      FMSTR_Poll();
+  }
+}
